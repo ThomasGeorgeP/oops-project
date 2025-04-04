0724ce6734c59b4384d4e2b6ae9e0bc34d7abbfe
Thomas George Panicker <gpthomas101@gmail.com>
2025-04-04 04:17:25 +0000

    Basic cross file inclusion testing

---
+++ library_management.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "test.h"
+#include <string>
+
+using namespace std;
+
+int main(){
+    book a;
+    
+    cout<<"Enter book name: ";
+    getline(cin,a.name);
+    cout<<"Book name: "<<a.name<<"\n";
+    return 0;
+}
---
+++ test.h
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+class book{
+    public:
+    string name;
+    string author;
+    string publisher;
+    float price;
+    int number_of_pages;
+};
+ 
