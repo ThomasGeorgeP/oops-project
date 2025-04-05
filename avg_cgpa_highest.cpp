db526bde22912b9c35f21170d68b242769589721
Aiman-D <aiman.dhanani@gmail.com>
2025-04-05 18:44:16 +0530

    added avg_highest_cgpa

--- avg_cgpa_highest.cpp
+++ avg_cgpa_highest.cpp
@@ -3,63 +3,79 @@
 using namespace std;
 
 class Student {
-public:
+private:
     string name;
     float cgpa;
-};
 
-int main() {
-    int totalStudents;
+public:
+    void input(int index) {
+        cout << "Enter name of student " << index + 1 << ": ";
+        getline(cin, name);
 
-    while (true) {
-        cout << "Enter number of students: ";
-        cin >> totalStudents;
-        if (totalStudents > 0) break;
-        cout << "Number of students must be greater than zero.\n";
-    }
-
-    cin.ignore();
-
-    Student allStudents[100];
-    Student topStudents[100];
-    int topCount = 0;
-    float highestCgpa = 0, totalCgpa = 0;
-
-    for (int i = 0; i < totalStudents; i++) {
-        cout << "Enter name of student " << i + 1 << ": ";
-        getline(cin, allStudents[i].name);
-
-        float inputCgpa;
         while (true) {
-            cout << "Enter CGPA of " << allStudents[i].name << ": ";
-            cin >> inputCgpa;
-
-            if (inputCgpa >= 0 && inputCgpa <= 10) break;
+            cout << "Enter CGPA of " << name << ": ";
+            cin >> cgpa;
+            if (cgpa >= 0 && cgpa <= 10) break;
             cout << "CGPA must be between 0 and 10. Please enter again.\n";
         }
 
-        allStudents[i].cgpa = inputCgpa;
-        cin.ignore();
+        cin.ignore(); 
+    }
 
-        totalCgpa += inputCgpa;
+    float getCgpa() const {
+        return cgpa;
+    }
 
-        if (i == 0 || inputCgpa > highestCgpa) {
-            highestCgpa = inputCgpa;
-            topCount = 1;
-            topStudents[0] = allStudents[i];
-        } else if (inputCgpa == highestCgpa) {
-            topStudents[topCount] = allStudents[i];
-            topCount++;
+    string getName() const {
+        return name;
+    }
+
+    static void run() {
+        int totalStudents;
+
+        while (true) {
+            cout << "Enter number of students: ";
+            cin >> totalStudents;
+            if (totalStudents > 0) break;
+            cout << "Number of students must be greater than zero.\n";
         }
+
+        cin.ignore(); // clear buffer
+
+        Student all[100];
+        Student top[100];
+        int topCount = 0;
+        float totalCgpa = 0, highestCgpa = 0;
+
+        for (int i = 0; i < totalStudents; i++) {
+            all[i].input(i);
+            float cg = all[i].getCgpa();
+            totalCgpa += cg;
+
+            if (i == 0 || cg > highestCgpa) {
+                highestCgpa = cg;
+                topCount = 1;
+                top[0] = all[i];
+            } else if (cg == highestCgpa) {
+                top[topCount] = all[i];
+                topCount++;
+            }
+        }
+
+        cout << "\nStudent(s) with highest CGPA:\n";
+        for (int i = 0; i < topCount; i++) {
+            cout << "Name: " << top[i].getName() << ", CGPA: " << top[i].getCgpa() << endl;
+        }
+
+        float avgCgpa = totalCgpa / totalStudents;
+        cout << "\nAverage CGPA: " << avgCgpa << endl;
     }
+};
 
-    cout << "\nStudent(s) with highest CGPA:\n";
-    for (int i = 0; i < topCount; i++) {
-        cout << "Name: " << topStudents[i].name << ", CGPA: " << topStudents[i].cgpa << endl;
-    }
 
-    float averageCgpa = totalCgpa / totalStudents;
-    cout << "\nAverage CGPA: " << averageCgpa << endl;
 
+int main() 
+{
+    Student::run();
     return 0;
 }
